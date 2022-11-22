07a61a7af132d4ceec03639f42d61fecaa249753
Philipp Dittrich <mail@philippdittri.ch>
2022-11-22 15:18:11 +0300

    ex 4.9

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -17,9 +17,15 @@ beforeEach(async() => {
 test('blog api returns the expected number of blogs', async () => {
   const response = await api
     .get('/api/blogs')
-    .expect(200)
     
   expect(response.body).toHaveLength(2)
+}, 100000)
+
+test('expect the unique identifier to be named id', async () => {
+  const response = await api
+    .get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
 }, 100000)
 
 afterAll(() => {
