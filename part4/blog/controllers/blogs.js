57e1358279355a478d53fad4cbc30d92405aa8da
Philipp Dittrich <mail@philippdittri.ch>
2022-11-24 15:17:26 +0300

    ex 4.17

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,19 +1,26 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user')
+    
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findOne({})
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id,
   })
 
   if(!blog.title || !blog.url) {
@@ -23,6 +30,10 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
